bb26ca7dffb222f5804fa8bee7297e68a13a9c4a
hhartono-mbp13 <hans@inerre.com>
2019-09-19 23:40:28 +0700

    contact us

--- application/controllers/Contact_us.php
+++ application/controllers/Contact_us.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Contact extends CI_Controller {
+class Contact_us extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
