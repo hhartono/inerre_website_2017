4b2568aa784d1af6e3f41fb61f84ac66764382b1
hhartono-mbp13 <hans@inerre.com>
2020-04-04 15:58:57 +0700

    wa floating

--- application/views/pano.php
+++ application/views/pano.php
@@ -229,10 +229,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             </div>
         </div>
         -->
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-            <a href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." class="float" target="_blank">
-                <i class="fa fa-whatsapp my-float"></i>
-            </a>
+
+        <!-- FLOATING WA BUTTON -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <a href="https://wa.me/6281311234455?text=Hello%20INERRE!%20I%20have%20a%20question%20about%20your%20interior%20design%20service." class="float" target="_blank">
+            <i class="fa fa-whatsapp my-float"></i>
+        </a>
+
         <!-- FOOTER 1 -->
         <footer id="footer1" class="page-section text-center pt-100-b-80-cont">
             <div class="container">
