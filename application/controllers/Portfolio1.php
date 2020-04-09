0a086fd0b75e7c2d352b43c5de1578da1f8f2d25
hhartono-mbp13 <hans@inerre.com>
2020-04-09 23:27:30 +0700

    fix

--- application/controllers/Portfolio1.php
+++ application/controllers/Portfolio1.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Portfolio extends CI_Controller {
+class Portfolio1 extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
