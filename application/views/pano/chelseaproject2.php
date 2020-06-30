492f7c789bff4de85b7d331479281defe9e68cd3
hhartono-mbp13 <hans@inerre.com>
2020-06-30 20:48:12 +0700

    krpano update

--- application/views/pano/chelseaproject2.php
+++ application/views/pano/chelseaproject2.php
@@ -184,7 +184,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             </div>
                             <div class="cis-text demo-buttons">
                                 <h3><span class="bold">CHELSEA - TASIKMALAYA - LIVING & PANTRY - REV1</span></h3>
-                                <a class="button medium thin hover-dark" href="http://18.139.219.177/chelseavtour/tour.html">see 360 rendering</a>
+                                <a class="button medium thin hover-dark" href="http://18.139.219.177/chelseavtour_rev1/tour.html">see 360 rendering</a>
                             </div>
                         </div>
                     </div>
