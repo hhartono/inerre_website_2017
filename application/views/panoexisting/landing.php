b07c9455ca8e7939619c1be5634e5fab073cba4f
hhartono-mbp13 <hans@inerre.com>
2020-07-03 21:46:57 +0700

    rinny eksisting uploaded

--- application/views/panoexisting/landing.php
+++ application/views/panoexisting/landing.php
@@ -195,6 +195,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <div class="icon icon-basic-pin1"></div>
                             </div>
                             <div class="cis-text">
+                                <h3><span class="bold">RINNY - CINERE</span></h3>
+                                <a class="button medium thin hover-dark" href="http://18.139.219.177/existing/rinny/tour.html">see existing</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
                                 <h3><span class="bold">ROBERT - KOPO</span></h3>
                                 <a class="button medium thin hover-dark" href="http://18.139.219.177/existing/robert/tour.html">see existing</a>
                             </div>
