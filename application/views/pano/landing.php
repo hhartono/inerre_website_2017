a28a52edcdacae72dd315bf1091fffd611bd1f66
hhartono-mbp13 <hans@inerre.com>
2020-07-22 22:32:03 +0700

    list update

--- application/views/pano/landing.php
+++ application/views/pano/landing.php
@@ -200,6 +200,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <a class="button medium thin hover-dark" href="<?php echo site_url('Pano/marco_project_rev1'); ?>">see project</a>
                             </div>
                         </div>
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
+                                <h3><span class="bold">RINNY - CINERE</span></h3>
+                                <a class="button medium thin hover-dark" href="<?php echo site_url('Pano/rinny_project'); ?>">see project</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
