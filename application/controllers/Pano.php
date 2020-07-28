a15c439beca2e23cceda16bdde7e34ede7c80c57
hhartono <hanskristian.1989@gmail.com>
2020-07-28 22:42:01 +0700

    ibu rinny all krpano - online

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -74,13 +74,17 @@ class Pano extends CI_Controller {
 
     public function rinny_project()
     {
-        $data['rows'][0]['title'] = "RINNY - CINERE - Master Bedroom";
-        $data['rows'][0]['url'] = "http://18.139.219.177/rinnyrtuvtour/tour.html";
+        $data['rows'][0]['title'] = "RINNY - CINERE - All Rooms";
+        $data['rows'][0]['url'] = "http://18.139.219.177/rinnyvtour/tour.html";
         $data['rows'][0]['button_title'] = "see 360 rendering";
 
-        $data['rows'][1]['title'] = "RINNY - CINERE - Kids Bedroom 1";
-        $data['rows'][1]['url'] = "http://18.139.219.177/rinnyrtavtour/tour.html";
+        $data['rows'][1]['title'] = "RINNY - CINERE - Master Bedroom";
+        $data['rows'][1]['url'] = "http://18.139.219.177/rinnyrtuvtour/tour.html";
         $data['rows'][1]['button_title'] = "see 360 rendering";
+
+        $data['rows'][2]['title'] = "RINNY - CINERE - Kids Bedroom 1";
+        $data['rows'][2]['url'] = "http://18.139.219.177/rinnyrtavtour/tour.html";
+        $data['rows'][2]['button_title'] = "see 360 rendering";
 
         $this->load->helper('url');
         $this->load->view('pano/basic_project_view', $data);
