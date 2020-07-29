b278113cbde1ae46ea50ef004f940affe5d00503
hhartono <hanskristian.1989@gmail.com>
2020-07-29 22:58:09 +0700

    fabiola update

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -90,6 +90,16 @@ class Pano extends CI_Controller {
         $this->load->view('pano/basic_project_view', $data);
     }
 
+    public function fabiola_project()
+    {
+        $data['rows'][0]['title'] = "FABIOLA - TASIKMALAYA - Pantry & Dining";
+        $data['rows'][0]['url'] = "http://18.139.219.177/fabioladiningpantryvtour/tour.html";
+        $data['rows'][0]['button_title'] = "see 360 rendering";
+
+        $this->load->helper('url');
+        $this->load->view('pano/basic_project_view', $data);
+    }
+
     public function test_eksisting()
     {
         $this->load->helper('url');
