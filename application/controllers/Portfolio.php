b16c5b5165112f83f0e3956b55590d8c5c7e06cb
hhartono <hanskristian.1989@gmail.com>
2020-08-01 18:45:23 +0700

    yc

--- application/controllers/Portfolio.php
+++ application/controllers/Portfolio.php
@@ -43,7 +43,7 @@ class Portfolio extends CI_Controller {
         $data['location'] = 'HEGARMANAH RESIDENCE, BANDUNG, INDONESIA';
 
         $data['directory_name'] = 'portfoliopage3';
-        $data['total_photo'] = '2';
+        $data['total_photo'] = '3';
 
         $this->load->helper('url');
         $this->load->view('portfolio/portfolio_basic', $data);
