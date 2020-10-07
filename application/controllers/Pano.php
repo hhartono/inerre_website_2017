d2f85147d5b1fd61d9972d5492f7031cea726f10
hhartono <hanskristian.1989@gmail.com>
2020-10-07 21:55:10 +0700

    puja uploaded

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -240,6 +240,16 @@ class Pano extends CI_Controller {
         $this->load->view('pano/basic_project_view', $data);
     }
 
+    public function puja_project()
+    {
+        $data['rows'][0]['title'] = "PUJA - 360 RENDERING PHASE 1";
+        $data['rows'][0]['url'] = "http://18.139.219.177/pujavtour/tour.html";
+        $data['rows'][0]['button_title'] = "see 360 rendering";
+
+        $this->load->helper('url');
+        $this->load->view('pano/basic_project_view', $data);
+    }
+
     public function test_eksisting()
     {
         $this->load->helper('url');
