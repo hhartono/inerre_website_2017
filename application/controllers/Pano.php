43ff50843b326922f7fcadf2573af6ca45ac6ba3
hhartono <hanskristian.1989@gmail.com>
2020-10-15 21:51:33 +0700

    soegiharto

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -160,6 +160,16 @@ class Pano extends CI_Controller {
         $this->load->view('pano/basic_project_view', $data);
     }
 
+    public function soegiharto_project_rev3()
+    {
+        $data['rows'][0]['title'] = "SOEGIHARTO - GREEN GARDEN - Master Bedroom & WIC - REVISION 3";
+        $data['rows'][0]['url'] = "http://18.139.219.177/soegihartortuvtour3/tour.html";
+        $data['rows'][0]['button_title'] = "see 360 rendering";
+
+        $this->load->helper('url');
+        $this->load->view('pano/basic_project_view', $data);
+    }
+
     public function denny_project()
     {
         $data['rows'][0]['title'] = "DENNY - DESIGN PRESENTATION";
