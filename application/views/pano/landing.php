cf569af4ce53c0b553dbce8165102271d202129e
hhartono <hanskristian.1989@gmail.com>
2020-11-11 09:44:36 +0700

    ferry

--- application/views/pano/landing.php
+++ application/views/pano/landing.php
@@ -233,6 +233,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <div class="icon icon-basic-pin1"></div>
                             </div>
                             <div class="cis-text">
+                                <h3><span class="bold">FERRY - CIPAGANTI</span></h3>
+                                <a class="button medium thin hover-dark" href="<?php echo site_url('Pano/ferry_project'); ?>">see project</a>
+                            </div>
+                        </div>
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
                                 <h3><span class="bold">GLENN - TITISWARI</span></h3>
                                 <a class="button medium thin hover-dark" href="<?php echo site_url('Pano/glenn_project'); ?>">see project</a>
                             </div>
