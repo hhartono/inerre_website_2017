c12c1156db849765d5dd4a432c70730b42c746ad
hhartono <hanskristian.1989@gmail.com>
2021-01-21 20:47:26 +0700

    lusiana project

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -366,6 +366,16 @@ class Pano extends CI_Controller {
         $this->load->view('pano/basic_project_view', $data);
     }
 
+    public function lusiana_project()
+    {
+        $data['rows'][0]['title'] = "LUSIANA - 360 RENDERING - PHASE 1";
+        $data['rows'][0]['url'] = "http://18.139.219.177/lusianavtour/tour.html";
+        $data['rows'][0]['button_title'] = "see 360 rendering";
+
+        $this->load->helper('url');
+        $this->load->view('pano/basic_project_view', $data);
+    }
+
     public function test_eksisting()
     {
         $this->load->helper('url');
