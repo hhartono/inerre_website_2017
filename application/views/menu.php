b670e4009864842f59721aa9fd36cbff741c276a
hhartono <hanskristian.1989@gmail.com>
2021-01-31 22:44:38 +0700

    synology

--- application/views/menu.php
+++ application/views/menu.php
@@ -245,6 +245,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         </div>
                     </div>
                 </div>
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text demo-buttons">
+                                <h3><span class="bold">INERRE Synology (web)</span></h3>
+                                <a class="button medium thin hover-dark" href="http://inerre.synology.me:5000/?launchApp=SYNO.SDS.Drive.Application">access INERRE Synology</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
 
