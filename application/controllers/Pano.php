80665a0d141f49210177a021d48d9399614914be
hhartono <hanskristian.1989@gmail.com>
2021-03-19 11:26:13 +0700

    william update

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -474,9 +474,13 @@ class Pano extends CI_Controller {
 
     public function william_project()
     {
-        $data['rows'][0]['title'] = "WILLIAM - 360 RENDERING - PHASE 1";
-        $data['rows'][0]['url'] = "http://18.139.219.177/williamvtour/tour.html";
+        $data['rows'][0]['title'] = "WILLIAM - DESIGN PRESENTATION - PHASE 1";
+        $data['rows'][0]['url'] = "http://18.139.219.177/pdf/william/William_Presentation_19.03.2021.pdf";
         $data['rows'][0]['button_title'] = "see 360 rendering";
+
+        $data['rows'][1]['title'] = "WILLIAM - 360 RENDERING - PHASE 1";
+        $data['rows'][1]['url'] = "http://18.139.219.177/williamvtour/tour.html";
+        $data['rows'][1]['button_title'] = "see 360 rendering";
 
         $this->load->helper('url');
         $this->load->view('pano/basic_project_view', $data);
