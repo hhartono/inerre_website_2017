fa812369f6abcfeca7e6431b468c1b8a772dfa90
hhartono <hanskristian.1989@gmail.com>
2021-04-27 23:04:34 +0700

    ellen & william eksisting

--- application/views/panoexisting/landing.php
+++ application/views/panoexisting/landing.php
@@ -204,7 +204,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         </div>
                     </div>
                 </div>
-                
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
+                                <h3><span class="bold">ELLEN - PANTAI MUTIARA</span></h3>
+                                <a class="button medium thin hover-dark" href="http://18.139.219.177/existing/ellen/tour.html">see existing</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="row">
                     <div class="col-md-4 col-sm-6">
                         <div class="cis-cont">
@@ -359,6 +373,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     </div>
                 </div>
 
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
+                                <h3><span class="bold">WILLIAM - KBP</span></h3>
+                                <a class="button medium thin hover-dark" href="http://18.139.219.177/existing/william/tour.html">see existing</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
                 <div class="row">
                     <div class="col-md-4 col-sm-6">
