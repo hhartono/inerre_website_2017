8079b91c37bd46c0f4b2805f5d4b163f8ff893ec
hhartono <hanskristian.1989@gmail.com>
2021-08-06 10:20:57 +0700

    jane presentation

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -660,6 +660,10 @@ class Pano extends CI_Controller {
         $data['rows'][0]['url'] = "http://18.139.219.177/janevtour1/tour.html";
         $data['rows'][0]['button_title'] = "see 360 rendering";
 
+        $data['rows'][1]['title'] = "JANE - PRESENTATION - PHASE 1";
+        $data['rows'][1]['url'] = "http://18.139.219.177/pdf/jane/Jane_Presentation_06.08.2021.pdf";
+        $data['rows'][1]['button_title'] = "download PDF document";
+
         $this->load->helper('url');
         $this->load->view('pano/basic_project_view', $data);
     }
