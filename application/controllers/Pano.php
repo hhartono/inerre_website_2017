978aee0283f4cdc27887be4240dc76ec4fc3ef0a
hhartono <hanskristian.1989@gmail.com>
2021-08-25 14:30:17 +0700

    shen rev3

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -492,13 +492,21 @@ class Pano extends CI_Controller {
         //$data['rows'][0]['button_title'] = "see presentation";
 
         // REV3
-        $data['rows'][0]['title'] = "MR. SHEN - INTERACTIVE PANTRY PRESENTATION - PHASE 1 - REV3 (BEST VIEWED USING PC or MAC)";
-        $data['rows'][0]['url'] = "https://view.genial.ly/60f51e9b18617d0fadf929c2/interactive-image-mr-shenisometric-detail-pantry19072021";
-        $data['rows'][0]['button_title'] = "see presentation";
+        $data['rows'][0]['title'] = "MR. SHEN - 360 RENDERING - PHASE 1 - REV3";
+        $data['rows'][0]['url'] = "http://18.139.219.177/shenvtour/tour.html";
+        $data['rows'][0]['button_title'] = "see 360 rendering";
 
-        $data['rows'][1]['title'] = "MR. SHEN - 360 RENDERING - PHASE 1 - REV2";
-        $data['rows'][1]['url'] = "http://18.139.219.177/shenvtour/tour.html";
-        $data['rows'][1]['button_title'] = "see 360 rendering";
+        $data['rows'][1]['title'] = "MR. SHEN - MATERIAL PRESENTATION - PHASE 1 - REV3";
+        $data['rows'][1]['url'] = "http://18.139.219.177/pdf/shen/MrShen_Material_25.08.2021.pdf";
+        $data['rows'][1]['button_title'] = "download PDF document";
+
+        $data['rows'][2]['title'] = "MR. SHEN - ISLAND WIRING DIAGRAM - PHASE 1 - REV3";
+        $data['rows'][2]['url'] = "http://18.139.219.177/pdf/shen/MrShen_Island_Wiring_25.08.2021.pdf";
+        $data['rows'][2]['button_title'] = "download PDF document";
+
+        $data['rows'][3]['title'] = "MR. SHEN - INTERACTIVE PANTRY PRESENTATION - PHASE 1 - REV3 (BEST VIEWED USING PC or MAC)";
+        $data['rows'][3]['url'] = "https://view.genial.ly/60f51e9b18617d0fadf929c2/interactive-image-mr-shenisometric-detail-pantry19072021";
+        $data['rows'][3]['button_title'] = "see presentation";
 
         $this->load->helper('url');
         $this->load->view('pano/basic_project_view', $data);
