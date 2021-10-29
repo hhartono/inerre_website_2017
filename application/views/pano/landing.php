26824224de838c1be0c93d59ac952cc7428b0bd9
hhartono <hanskristian.1989@gmail.com>
2021-10-29 15:19:04 +0700

    sohpia

--- application/views/pano/landing.php
+++ application/views/pano/landing.php
@@ -494,7 +494,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <div class="icon icon-basic-pin1"></div>
                             </div>
                             <div class="cis-text">
-                                <h3><span class="bold">SOHPIA - GREEN GARDEN</span></h3>
+                                <h3><span class="bold">SOHPIA - PALEMBANG</span></h3>
                                 <a class="button medium thin hover-dark" href="<?php echo site_url('Pano/sohpia_project'); ?>">see project</a>
                             </div>
                         </div>
