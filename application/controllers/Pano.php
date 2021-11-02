c29f15edd70c82e21b667f2ec2d640922dfc5c38
hhartono <hanskristian.1989@gmail.com>
2021-11-02 21:21:18 +0700

    sohpia

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -732,14 +732,18 @@ class Pano extends CI_Controller {
 
     public function sohpia_project()
     {
-        $data['rows'][0]['title'] = "SOHPIA - 360 RENDERING - PHASE 1";
+        $data['rows'][0]['title'] = "SOHPIA - 360 RENDERING - PHASE 1 - ALT 1";
         $data['rows'][0]['url'] = "http://18.139.219.177/sohpiavtour/tour.html";
         $data['rows'][0]['button_title'] = "see 360 rendering";
 
-        $data['rows'][1]['title'] = "SOHPIA - PRESENTATION - PHASE 1";
+        $data['rows'][1]['title'] = "SOHPIA - PRESENTATION - PHASE 1 - ALT 1";
         $data['rows'][1]['url'] = "http://18.139.219.177/pdf/sohpia/Sohpia_Presentation_29.10.2021.pdf";
         $data['rows'][1]['button_title'] = "download PDF document";
 
+        $data['rows'][0]['title'] = "SOHPIA - 360 RENDERING - PHASE 1 - ALT 2";
+        $data['rows'][0]['url'] = "http://18.139.219.177/sohpiavtour2/tour.html";
+        $data['rows'][0]['button_title'] = "see 360 rendering";
+        
         $this->load->helper('url');
         $this->load->view('pano/basic_project_view', $data);
     }
