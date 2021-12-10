e8d9ab4cff33b8a49b4d813a56cc83d33863e780
hhartono <hanskristian.1989@gmail.com>
2021-12-10 09:15:06 +0700

    sumarko

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -734,6 +734,20 @@ class Pano extends CI_Controller {
         $this->load->view('pano/basic_project_view', $data);
     }
 
+    public function sumarko_project_2()
+    {
+        $data['rows'][0]['title'] = "SUMARKO - 360 RENDERING - PHASE 2";
+        $data['rows'][0]['url'] = "http://18.139.219.177/sumarkovtour2/tour.html";
+        $data['rows'][0]['button_title'] = "see 360 rendering";
+
+        $data['rows'][1]['title'] = "SUMARKO - PRESENTATION - PHASE 2";
+        $data['rows'][1]['url'] = "http://18.139.219.177/pdf/sumarko/Sumarko_Presentation_10.12.2021.pdf";
+        $data['rows'][1]['button_title'] = "see 360 rendering";
+
+        $this->load->helper('url');
+        $this->load->view('pano/basic_project_view', $data);
+    }
+
     public function wandi_project()
     {
         $data['rows'][0]['title'] = "WANDI - 360 RENDERING - PHASE 1 -  REV1";
