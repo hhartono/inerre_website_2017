066b9707d850c9c2554e9dd8a52d8612d951563e
hhartono <hanskristian.1989@gmail.com>
2021-12-10 23:07:37 +0700

    sumarko fase 2

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -740,9 +740,9 @@ class Pano extends CI_Controller {
         $data['rows'][0]['url'] = "http://18.139.219.177/sumarkovtour2/tour.html";
         $data['rows'][0]['button_title'] = "see 360 rendering";
 
-        //$data['rows'][1]['title'] = "SUMARKO - PRESENTATION - PHASE 2";
-        //$data['rows'][1]['url'] = "http://18.139.219.177/pdf/sumarko/Sumarko_Presentation_10.12.2021.pdf";
-        //$data['rows'][1]['button_title'] = "download PDF document";
+        $data['rows'][1]['title'] = "SUMARKO - PRESENTATION - PHASE 2";
+        $data['rows'][1]['url'] = "http://18.139.219.177/pdf/sumarko/Sumarko_Presentation_09.12.2021.pdf";
+        $data['rows'][1]['button_title'] = "download PDF document";
 
         $this->load->helper('url');
         $this->load->view('pano/basic_project_view', $data);
