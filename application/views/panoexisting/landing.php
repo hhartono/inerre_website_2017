5601121fd099fbbe405ce7d74197727d03ee5978
hhartono <hanskristian.1989@gmail.com>
2022-02-17 15:52:30 +0700

    abigail

--- application/views/panoexisting/landing.php
+++ application/views/panoexisting/landing.php
@@ -170,6 +170,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <div class="icon icon-basic-pin1"></div>
                             </div>
                             <div class="cis-text">
+                                <h3><span class="bold">ABIGAIL - BANDUNG</span></h3>
+                                <a class="button medium thin hover-dark" href="http://18.139.219.177/existing/abigail/tour.html">see existing</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
                                 <h3><span class="bold">ADELINE - SETRA DUTA</span></h3>
                                 <a class="button medium thin hover-dark" href="http://18.139.219.177/existing/adeline/tour.html">see existing</a>
                             </div>
