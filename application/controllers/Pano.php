8164f798f3ebfb6a8740e3a90f19f9afd3089c23
hhartono <hanskristian.1989@gmail.com>
2022-03-30 21:14:35 +0700

    rinny

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -128,6 +128,16 @@ class Pano extends CI_Controller {
         $this->load->view('pano/basic_project_view', $data);
     }
 
+    public function rinny_project_2()
+    {
+        $data['rows'][0]['title'] = "RINNY - CINERE - Grandkids Bedroom";
+        $data['rows'][0]['url'] = "http://18.139.219.177/rinnyrtcvtour/tour.html";
+        $data['rows'][0]['button_title'] = "see 360 rendering";
+
+        $this->load->helper('url');
+        $this->load->view('pano/basic_project_view', $data);
+    }
+
     public function fabiola_project()
     {
         $data['rows'][0]['title'] = "FABIOLA - TASIKMALAYA - Pantry & Dining";
--- application/views/pano/landing.php
+++ application/views/pano/landing.php
@@ -530,6 +530,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <div class="icon icon-basic-pin1"></div>
                             </div>
                             <div class="cis-text">
+                                <h3><span class="bold">RINNY - CINERE - FASE2</span></h3>
+                                <a class="button medium thin hover-dark" href="<?php echo site_url('Pano/rinny_project_2'); ?>">see project</a>
+                            </div>
+                        </div>
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
                                 <h3><span class="bold">SARIWATI - ANCOL</span></h3>
                                 <a class="button medium thin hover-dark" href="<?php echo site_url('Pano/sariwati_project'); ?>">see project</a>
                             </div>
