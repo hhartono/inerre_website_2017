256f0ea6335860c8b13d470db37c173d7c20fa28
hhartono <hanskristian.1989@gmail.com>
2022-04-21 14:04:14 +0700

    regina

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -1071,6 +1071,16 @@ class Pano extends CI_Controller {
         $this->load->view('pano/basic_project_view', $data);
     }
 
+    public function regina_project()
+    {
+        $data['rows'][0]['title'] = "REGINA - 360 RENDERING";
+        $data['rows'][0]['url'] = "http://18.139.219.177/reginavtour/tour.html";
+        $data['rows'][0]['button_title'] = "see 360 rendering";
+
+        $this->load->helper('url');
+        $this->load->view('pano/basic_project_view', $data);
+    }
+
     public function sample_idea()
     {
         $data['rows'][0]['title'] = "SAMPLE IDEA 1 - 360 RENDERING";
