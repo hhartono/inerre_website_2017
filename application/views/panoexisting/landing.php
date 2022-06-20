05e6b37d307cd91bdecf6254f9b6e91d8051dba1
hhartono <hanskristian.1989@gmail.com>
2022-06-20 17:12:00 +0700

    leny 2

--- application/views/panoexisting/landing.php
+++ application/views/panoexisting/landing.php
@@ -380,6 +380,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <div class="icon icon-basic-pin1"></div>
                             </div>
                             <div class="cis-text">
+                                <h3><span class="bold">LENY 2 - DUTA GARDEN - UPDATE 17 JUNE 2022</span></h3>
+                                <a class="button medium thin hover-dark" href="http://18.139.219.177/existing/leny2/tour.html">see existing</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
                                 <h3><span class="bold">LUSIANA - GURUMINDA</span></h3>
                                 <a class="button medium thin hover-dark" href="http://18.139.219.177/existing/lusiana/tour.html">see existing</a>
                             </div>
