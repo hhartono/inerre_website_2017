9c424ec000f7e00ed5665d942305208dff246c67
hhartono <hanskristian.1989@gmail.com>
2022-09-09 18:06:15 +0700

    desi

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -1096,6 +1096,20 @@ class Pano extends CI_Controller
         $this->load->view('pano/basic_project_view', $data);
     }
 
+    public function desi_project()
+    {
+        $data['rows'][0]['title'] = "IBU DESI - 360 RENDERING - MASTER BEDROOM";
+        $data['rows'][0]['url'] = "http://18.139.219.177/desivtour/tour.html";
+        $data['rows'][0]['button_title'] = "see 360 rendering";
+
+        //$data['rows'][1]['title'] = "JANE - PRESENTATION - PHASE 1";
+        //$data['rows'][1]['url'] = "http://18.139.219.177/pdf/jane/Jane_Presentation_24.08.2021.pdf";
+        //$data['rows'][1]['button_title'] = "download PDF document";
+
+        $this->load->helper('url');
+        $this->load->view('pano/basic_project_view', $data);
+    }
+
     public function adeline_project()
     {
         $data['rows'][0]['title'] = "ADELINE - 360 RENDERING - RTA";
