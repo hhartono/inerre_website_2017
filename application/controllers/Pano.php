4f1fee746ed216d642b95706dc076ff5deea663f
hhartono <hanskristian.1989@gmail.com>
2022-09-14 14:25:26 +0700

    desi

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -1102,9 +1102,9 @@ class Pano extends CI_Controller
         $data['rows'][0]['url'] = "http://18.139.219.177/desivtour/tour.html";
         $data['rows'][0]['button_title'] = "see 360 rendering";
 
-        //$data['rows'][1]['title'] = "JANE - PRESENTATION - PHASE 1";
-        //$data['rows'][1]['url'] = "http://18.139.219.177/pdf/jane/Jane_Presentation_24.08.2021.pdf";
-        //$data['rows'][1]['button_title'] = "download PDF document";
+        $data['rows'][1]['title'] = "IBU DESI - PRESENTATION - PHASE 1";
+        $data['rows'][1]['url'] = "http://18.139.219.177/pdf/desi/Desi_presentation_13.09.2022.pdf";
+        $data['rows'][1]['button_title'] = "download PDF document";
 
         $this->load->helper('url');
         $this->load->view('pano/basic_project_view', $data);
