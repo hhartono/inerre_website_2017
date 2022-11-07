8f8696a8d241ba179c2abfa8b8fd404309af703e
hhartono <hanskristian.1989@gmail.com>
2022-11-07 09:35:19 +0700

    sohpia

--- application/views/panoexisting/landing.php
+++ application/views/panoexisting/landing.php
@@ -605,6 +605,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <div class="icon icon-basic-pin1"></div>
                             </div>
                             <div class="cis-text">
+                                <h3><span class="bold">SOHPIA - PALEMBANG - UPDATE 4 Nov 2022</span></h3>
+                                <a class="button medium thin hover-dark" href="http://18.139.219.177/existing/sohpia3/tour.html">see existing</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
                                 <h3><span class="bold">SRI SATRIATY - BANDUNG</span></h3>
                                 <a class="button medium thin hover-dark" href="http://18.139.219.177/existing/srisatriaty/tour.html">see existing</a>
                             </div>
