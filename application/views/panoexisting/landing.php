202d86b712052637dfadc64e7ba2bb9de590cd4f
hhartono <hanskristian.1989@gmail.com>
2023-04-12 09:54:13 +0700

    erland

--- application/views/panoexisting/landing.php
+++ application/views/panoexisting/landing.php
@@ -254,6 +254,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <div class="icon icon-basic-pin1"></div>
                             </div>
                             <div class="cis-text">
+                                <h3><span class="bold">ERLAND - BOGOR</span></h3>
+                                <a class="button medium thin hover-dark" href="http://18.139.219.177/existing/erland/tour.html">see existing</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
                                 <h3><span class="bold">ERNI - BSD</span></h3>
                                 <a class="button medium thin hover-dark" href="http://18.139.219.177/existing/erni/tour.html">see existing</a>
                             </div>
