90a171e6538c60de5209f23bd9dfc3a508b162a3
hhartono <hanskristian.1989@gmail.com>
2023-06-22 12:41:49 +0700

    bais

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -1123,7 +1123,7 @@ class Pano extends CI_Controller
         $data['rows'][0]['button_title'] = "see 360 rendering";
 
         $data['rows'][1]['title'] = "BAIS - PRESENTATION - LIBRARY";
-        $data['rows'][1]['url'] = "http://18.139.219.177/pdf/bais/BAIS_Presentation_13.06.2023.pdf";
+        $data['rows'][1]['url'] = "http://18.139.219.177/pdf/bais/BAIS_Presentation_20.06.2023.pdf";
         $data['rows'][1]['button_title'] = "download PDF document";
         
         $this->load->helper('url');
