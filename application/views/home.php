cf4390dd14831935edf325846522491fd7f3da1c
hhartono <hanskristian.1989@gmail.com>
2023-09-03 16:41:37 +0700

    gtag

--- application/views/home.php
+++ application/views/home.php
@@ -13,7 +13,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 -->
 
-<!-- Google Analytics -->
+<!-- Google Analytics - OLD
 <script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -23,6 +23,17 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-57299931-1', 'auto');
 ga('send', 'pageview');
 </script>
+-->
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=TAG_ID"></script>
+
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-57299931-1');
+</script>
+
 <!-- End Google Analytics -->
 
 <html>
