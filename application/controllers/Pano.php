38f565f85b470e68ccdf82288a0c743eda5186b4
hhartono <hanskristian.1989@gmail.com>
2023-09-14 20:17:36 +0700

    meiti

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -1192,6 +1192,10 @@ class Pano extends CI_Controller
         $data['rows'][0]['url'] = "http://18.139.219.177/meityvtour/tour.html";
         $data['rows'][0]['button_title'] = "see 360 rendering";
 
+        $data['rows'][1]['title'] = "IBU MEITY - 360 RENDERING - RTU PART 2";
+        $data['rows'][1]['url'] = "http://18.139.219.177/meityvtour2/tour.html";
+        $data['rows'][1]['button_title'] = "see 360 rendering";
+
         $this->load->helper('url');
         $this->load->view('pano/basic_project_view', $data);
     }
