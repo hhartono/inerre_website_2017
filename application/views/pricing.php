ee3eeaf5dec660921caea97d71c5a416ea86deaf
hhartono <hanskristian.1989@gmail.com>
2023-09-21 21:39:32 +0700

    pricing

--- application/views/pricing.php
+++ application/views/pricing.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: http://18.139.219.177/pdf/pricing/inerre_general_pricing.pdf"); /* Redirect browser */
+header("Location: http://" . $GLOBALS['inerre_vr'] . "/pdf/pricing/inerre_general_pricing.pdf"); /* Redirect browser */
 exit();
 ?>
