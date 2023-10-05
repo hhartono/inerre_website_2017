71a94ed453d373d4b70ce5fb267f4e992f34badf
hhartono <hanskristian.1989@gmail.com>
2023-10-05 22:08:46 +0700

    irene

--- application/views/panoexisting/landing.php
+++ application/views/panoexisting/landing.php
@@ -439,6 +439,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <div class="icon icon-basic-pin1"></div>
                             </div>
                             <div class="cis-text">
+                                <h3><span class="bold">IRENE - BSD</span></h3>
+                                <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr_backup']); ?>/existing/irene/tour.html">see existing</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
                                 <h3><span class="bold">JENSEN - PASIRKALIKI</span></h3>
                                 <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr']); ?>/existing/jensen/tour.html">see existing</a>
                             </div>
