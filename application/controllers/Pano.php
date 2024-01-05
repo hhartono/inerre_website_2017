57331cb23b309aa471ad39fac1afe1fe2a7059ea
hhartono <hanskristian.1989@gmail.com>
2024-01-05 19:36:44 +0700

    shen 7

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -619,6 +619,16 @@ class Pano extends CI_Controller
         $this->load->view('pano/basic_project_view', $data);
     }
 
+    public function shen_project_7()
+    {
+        $data['rows'][0]['title'] = "MR. SHEN - 360 RENDERING - PHASE 7 - MASTER BEDROOM";
+        $data['rows'][0]['url'] = "http://" . $GLOBALS['inerre_vr_backup'] . "/shenvtour8/tour.html";
+        $data['rows'][0]['button_title'] = "see 360 rendering";
+
+        $this->load->helper('url');
+        $this->load->view('pano/basic_project_view', $data);
+    }
+
     public function william_project()
     {
         $data['rows'][0]['title'] = "WILLIAM - DESIGN PRESENTATION - PHASE 1";
