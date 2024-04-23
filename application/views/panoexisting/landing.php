81e5f69bbe1a2580880f61d46e8690ecca6b11cc
hhartono <hanskristian.1989@gmail.com>
2024-04-23 11:31:35 +0700

    willy

--- application/views/panoexisting/landing.php
+++ application/views/panoexisting/landing.php
@@ -989,6 +989,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <div class="icon icon-basic-pin1"></div>
                             </div>
                             <div class="cis-text">
+                                <h3><span class="bold">WILLY - TALAGA BODAS</span></h3>
+                                <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr_backup']); ?>/existing/willy/tour.html">see existing</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
                                 <h3><span class="bold">WILLY SOENARYO - SETRA DUTA</span></h3>
                                 <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr']); ?>/existing/willysoenaryo/tour.html">see existing</a>
                             </div>
