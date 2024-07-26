819527937fd0fec92aee3e6334317245c0fde634
hhartono <hanskristian.1989@gmail.com>
2024-07-26 14:12:39 +0700

    ronald

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -1628,6 +1628,16 @@ class Pano extends CI_Controller
         $this->load->view('pano/basic_project_view', $data);
     }
 
+    public function ronald_project()
+    {
+        $data['rows'][0]['title'] = "RONALD - 360 RENDERING - MASTER BEDROOM";
+        $data['rows'][0]['url'] = "http://" . $GLOBALS['inerre_vr_backup'] . "/ronaldvtour/tour.html";
+        $data['rows'][0]['button_title'] = "see 360 rendering";
+
+        $this->load->helper('url');
+        $this->load->view('pano/basic_project_view', $data);
+    }
+
     public function rinaldo_project()
     {
         $data['rows'][0]['title'] = "RINALDO - 360 RENDERING - MASTER BEDROOM";
