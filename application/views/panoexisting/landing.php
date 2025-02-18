e62d905834117c53b6b90597ef2afcfc3193f5a8
hhartono <hanskristian.1989@gmail.com>
2025-02-18 20:50:43 +0700

    fred

--- application/views/panoexisting/landing.php
+++ application/views/panoexisting/landing.php
@@ -425,6 +425,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <div class="icon icon-basic-pin1"></div>
                             </div>
                             <div class="cis-text">
+                                <h3><span class="bold">FREDDY - KBP</span></h3>
+                                <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr_backup']); ?>/existing/freddy/tour.html">see existing</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
                                 <h3><span class="bold">GLENN - TITISWARI</span></h3>
                                 <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr']); ?>/existing/glenn/tour.html">see existing</a>
                             </div>
