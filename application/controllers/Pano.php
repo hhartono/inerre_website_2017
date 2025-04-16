3e942b4dc42f2952e69cc74905682f5c36e58bb8
hhartono <hanskristian.1989@gmail.com>
2025-04-16 13:06:42 +0700

    kids

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -1332,6 +1332,10 @@ class Pano extends CI_Controller
         $data['rows'][0]['url'] = "http://" . $GLOBALS['inerre_vr_backup'] . "/davidshellavtour/tour.html";
         $data['rows'][0]['button_title'] = "see 360 rendering";
 
+        $data['rows'][1]['title'] = "DAVID & SHELLA - 360 RENDERING - KIDS BEDROOM";
+        $data['rows'][1]['url'] = "http://" . $GLOBALS['inerre_vr_backup'] . "/davidshellavtour1/tour.html";
+        $data['rows'][1]['button_title'] = "see 360 rendering";
+
         $this->load->helper('url');
         $this->load->view('pano/basic_project_view', $data);
     }
