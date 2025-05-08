90be6d7974236b39268ba30a67a28dc561e611fe
hhartono <hanskristian.1989@gmail.com>
2025-05-08 20:05:41 +0700

    test

--- application/views/panoexisting/landing.php
+++ application/views/panoexisting/landing.php
@@ -327,7 +327,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <h3><span class="bold">DAVID SHELLA - PERMATA BUANA</span></h3>
                                 <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr_backup']); ?>/existing/davidshella/tour.html">see existing</a>
                                 <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr_backup']); ?>/existing/davidshella2/tour.html">see existing (23 04 2025)</a>
-                                <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr_backup']); ?>/existing/davidshella2/tour.html">see existing (06 05 2025)</a>
+                                <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr_backup']); ?>/existing/davidshella3/tour.html">see existing (06 05 2025)</a>
                             </div>
                         </div>
                     </div>
