d7d5144c29bf29b2c02363e1c75d0901bb94ca3a
hhartono <hanskristian.1989@gmail.com>
2025-06-06 12:56:43 +0700

    menu

--- application/controllers/Menu2025.php
+++ application/controllers/Menu2025.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Menu extends CI_Controller {
+class Menu2025 extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
