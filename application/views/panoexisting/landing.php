b4bddfd54d4f79a211dec29cb9345ff32d649f27
hhartono <hanskristian.1989@gmail.com>
2025-12-01 09:34:12 +0700

    BBB 5

--- application/views/panoexisting/landing.php
+++ application/views/panoexisting/landing.php
@@ -344,6 +344,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr_backup']); ?>/existing/davidshella2/tour.html">see existing (23 04 2025)</a>
                                 <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr_backup']); ?>/existing/davidshella3/tour.html">see existing (06 05 2025)</a>
                                 <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr_backup']); ?>/existing/davidshella4/tour.html">see existing (10 06 2025)</a>
+                                <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr_backup']); ?>/existing/davidshella5/tour.html">see existing (01 12 2025)</a>
                             </div>
                         </div>
                     </div>
