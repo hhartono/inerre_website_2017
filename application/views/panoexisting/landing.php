db5b3b407e06546dfe6cf692d8190c32dee6cc23
hhartono <hanskristian.1989@gmail.com>
2026-02-03 19:42:05 +0700

    sevelyn

--- application/views/panoexisting/landing.php
+++ application/views/panoexisting/landing.php
@@ -1087,6 +1087,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <div class="icon icon-basic-pin1"></div>
                             </div>
                             <div class="cis-text">
+                                <h3><span class="bold">SEVELYN - BUDI ASIH</span></h3>
+                                <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr_backup']); ?>/existing/sevelyn/tour.html">see existing</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="cis-cont">
+                            <div class="cis-icon">
+                                <div class="icon icon-basic-pin1"></div>
+                            </div>
+                            <div class="cis-text">
                                 <h3><span class="bold">SHOWROOM VISIT - JULY 2023</span></h3>
                                 <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr']); ?>/existing/showroomabodestyle/tour.html">Abode</a>
                                 <a class="button medium thin hover-dark" href="http://<?php echo urldecode($GLOBALS['inerre_vr']); ?>/existing/showroomcasaitalialiving/tour.html">Casa Italia</a>
