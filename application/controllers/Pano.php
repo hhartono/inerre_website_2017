7fee7f00c25a8fda4dedfd4c95ab6c2219797066
hhartono <hanskristian.1989@gmail.com>
2026-03-07 19:15:12 +0700

    alfred

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -1534,6 +1534,11 @@ class Pano extends CI_Controller
         $data['rows'][0]['url'] = "http://" . $GLOBALS['inerre_vr_backup'] . "/alfredvtour/tour.html";
         $data['rows'][0]['button_title'] = "see 360 rendering";
 
+        $data['rows'][1]['title'] = "ALFRED - LIVING, DINING, FOYER - 360 RENDERING";
+        $data['rows'][1]['url'] = "http://" . $GLOBALS['inerre_vr_backup'] . "/alfredvtour2/tour.html";
+        $data['rows'][1]['button_title'] = "see 360 rendering";
+
+
         $this->load->helper('url');
         $this->load->view('pano/basic_project_view', $data);
     }
