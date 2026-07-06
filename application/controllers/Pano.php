828442def6189a94a4d43d023b4c9cdac23e02f7
hhartono <hanskristian.1989@gmail.com>
2026-07-06 13:04:10 +0700

    rwp2

--- application/controllers/Pano.php
+++ application/controllers/Pano.php
@@ -1562,6 +1562,10 @@ class Pano extends CI_Controller
         $data['rows'][0]['url'] = "http://" . $GLOBALS['inerre_vr_backup'] . "/rwpvtour/tour.html";
         $data['rows'][0]['button_title'] = "see 360 rendering";
 
+        $data['rows'][1]['title'] = "RWP - LIVING LT 2 - 360 RENDERING";
+        $data['rows'][1]['url'] = "http://" . $GLOBALS['inerre_vr_backup'] . "/rwpvtour2/tour.html";
+        $data['rows'][1]['button_title'] = "see 360 rendering";
+        
         $this->load->helper('url');
         $this->load->view('pano/basic_project_view', $data);
     }
